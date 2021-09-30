4318cc06e214d95b4ba7469aa04d7fb8fb1be89e
SyedBajiShaik <syed.shaik@collabera.com>
2021-09-30 11:45:52 +0530

    till ere map add clients completed

--- js/script.js
+++ js/script.js
@@ -1,81 +1,90 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-  setTimeout(function(){
+  setTimeout(function () {
     $(".bgv_check_det").addClass("active");
-   }, 1000);
+  }, 1000);
 
-   $(".close_check_details").click(function(){
-     $(".bgv_check_det").removeClass("active");
-   })
+  $(".close_check_details").click(function () {
+    $(".bgv_check_det").removeClass("active");
+  })
 
-  $(".modal_open").click(function(e){
+  $(".modal_open").click(function (e) {
     e.stopPropagation();
     e.preventDefault();
     let modal = $(this).attr("data-modalName");
-    $("#"+modal+"").addClass("active");
+    $("#" + modal + "").addClass("active");
   })
 
-  $(".open_modal_check").click(function(e){
-    if($(this).children("input").prop("checked")){
+  $(".open_modal_check").click(function (e) {
+    if ($(this).children("input").prop("checked")) {
       console.log("checked");
       $(this).children("input").prop("checked", "true");
       let modal = $(this).attr("data-modalName");
-      $("#"+modal+"").addClass("active");
-    }else{
+      $("#" + modal + "").addClass("active");
+    } else {
       $(this).children("input").removeAttr("checked");
 
       console.log("not checked");
-      
+
     }
   })
 
-  $(".custom_modal .close").click(function(){
+  $(".custom_modal .close").click(function () {
     // console.log("close");
     $(this).closest(".custom_modal").removeClass("active");
   })
 
   // scroll_actions -active
-  $(".scroll_actions li span").click(function(){
+  $(".scroll_actions li span").click(function () {
     $(this).parent().parent().parent(".scroll_actions").toggleClass("active");
   })
 
   // profile_card_show
-  $(".open_popup").click(function(e){
+  $(".open_popup").click(function (e) {
     e.stopPropagation();
     e.preventDefault();
     $(this).children(".action_popup").toggleClass("active");
   });
-  $(".open_filter").click(function(e){
+  $(".open_filter").click(function (e) {
     e.stopPropagation();
     e.preventDefault();
     $(this).siblings(".filter_wrap").toggleClass("active");
   });
-  $(".open_popup_check input").change(function(){
+  $(".open_popup_check input").change(function () {
     $(this).siblings(".action_popup").toggleClass("active");
   })
 
-  $(".close_popup").click(function(e){
+  $(".close_popup").click(function (e) {
     e.stopPropagation();
     e.preventDefault();
     $(this).closest(".filter_wrap").removeClass("active");
     $(this).closest(".action_popup").removeClass("active");
   })
 
-  $(".popup_body, .popup_header").click(function(e){
+  $(".popup_body, .popup_header").click(function (e) {
     e.stopPropagation();
     e.preventDefault();
   })
-  
-  $(".docs_mainwrap p, .info_table .accordion-toggle").click(function(e){
+
+  $(".docs_mainwrap p, .info_table .accordion-toggle").click(function (e) {
     e.stopPropagation();
     e.preventDefault();
   })
 
   // sidebar add class
-  $(".side_menu").click(function() {
+  $(".side_menu").click(function () {
     $(".leftSideBar").toggleClass("active");
   })
+
+  $('.nav-tab').on('click', function () {
+    showActiveTabContent($(this));
+  });
+  function showActiveTabContent(thiss) {
+    var tabname = thiss.data('tab');
+    $('*[data-content], *[data-tab]').removeClass('active');
+    $('*[data-content="' + tabname + '"], *[data-tab="' + tabname + '"]').addClass('active');
+  }
 })
-$(function() {
+$(function () {
   $('.box').matchHeight();
 });
